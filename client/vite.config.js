e44bf2efc405845471bf356c2b17c53cbf7d8efe
Elmir <elmirdevland@gmail.com>
2024-09-17 16:58:51 +0400

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'https://elmirdevland.github.io/investcafe/',
   plugins: [react()],
   server: {
     host: true,
