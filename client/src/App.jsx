68b8b8193c4700fa2e8c18bd6d80a4f493c81f8f
Elmir <elmirdevland@gmail.com>
2024-09-19 07:41:00 +0400

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,16 +45,7 @@ function App() {
       window.history.replaceState({}, '', url.toString());
     }
 
-    const debounce = (func, wait) => {
-      let timeout;
-      return function (...args) {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), wait);
-      };
-    };
-
-    const handleScroll = debounce(() => {
+    const handleScroll = () => {
       let current = '';
 
       const navigationMenu = document.querySelector('.navigation-menu');
@@ -66,38 +57,23 @@ function App() {
         }
       }
 
-      let newActiveLink = null;
-
       sectionsRef.current.forEach((section) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
         if (window.pageYOffset >= sectionTop - sectionHeight / 3 - 100) {
           current = section.id;
-          newActiveLink = document.querySelector(`a[href="#${current}"]`);
         }
       });
 
       navLinksRef.current.forEach((link) => {
         link.classList.remove('active');
+        if (link.getAttribute('href') === current) {
+          link.classList.add('active');
+        }
       });
 
-      if (newActiveLink) {
-        newActiveLink.classList.add('active');
-
-        const linkPosition =
-          newActiveLink.getBoundingClientRect().left + window.scrollX;
-        const menuPosition =
-          navigationMenu.getBoundingClientRect().left + window.scrollX;
-        const offset = linkPosition - menuPosition;
-
-        navigationMenu.scrollTo({
-          left: offset,
-          behavior: 'smooth',
-        });
-      }
-
       setCurrentSection(current);
-    }, 100);
+    };
 
     window.addEventListener('scroll', handleScroll);
 
