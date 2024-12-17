8f9e31d8c755372051b5a6297e2428cd7613a2ca
Elmir <elmirdevland@gmail.com>
2024-12-17 11:33:51 +0400

    added images

---
+++ client/src/context/drinkImages.js
@@ -0,0 +1,85 @@
+import americano from '../assets/img/drinksimg/americano.png';
+import turkishcoffee from '../assets/img/drinksimg/turkish-coffee.png';
+import strawberrylemonade from '../assets/img/drinksimg/strawberry-lemonade.png';
+import strawberryicetea from '../assets/img/drinksimg/strawberry-icetea.png';
+import sprite from '../assets/img/drinksimg/sprite.png';
+import sparklingamericano from '../assets/img/drinksimg/sparkling-americano.png';
+import sirabstill from '../assets/img/drinksimg/sirab-still.png';
+import sirabsparkling from '../assets/img/drinksimg/sirab-sparkling.png';
+import sarikiz from '../assets/img/drinksimg/sarikiz.png';
+import raspberrylemonade from '../assets/img/drinksimg/raspberry-lemonade.png';
+import raspberryicetea from '../assets/img/drinksimg/raspberry-icetea.png';
+import pearlemonade from '../assets/img/drinksimg/pear-lemonade.png';
+import peachjuice from '../assets/img/drinksimg/peach-juice.png';
+import peachicetea from '../assets/img/drinksimg/peach-icetea.png';
+import passionfruitlemonade from '../assets/img/drinksimg/passionfruit-lemonade.png';
+import orangejuice from '../assets/img/drinksimg/orange-juice.png';
+import multijuice from '../assets/img/drinksimg/multi-juice.png';
+import mangolemonade from '../assets/img/drinksimg/mango-lemonade.png';
+import mangoicetea from '../assets/img/drinksimg/mango-icetea.png';
+import limelemonade from '../assets/img/drinksimg/lime-lemonade.png';
+import limeicetea from '../assets/img/drinksimg/lime-icetea.png';
+import lemonlemonade from '../assets/img/drinksimg/lemon-lemonade.png';
+import lemonicetea from '../assets/img/drinksimg/lemon-icetea.png';
+import latte from '../assets/img/drinksimg/latte.png';
+import jasminetea from '../assets/img/drinksimg/jasmine-tea.png';
+import icemocha from '../assets/img/drinksimg/ice-mocha.png';
+import icelatte from '../assets/img/drinksimg/ice-latte.png';
+import iceamericano from '../assets/img/drinksimg/ice-americano.png';
+import greentea from '../assets/img/drinksimg/green-tea.png';
+import fanta from '../assets/img/drinksimg/fanta.png';
+import espresso from '../assets/img/drinksimg/espresso.png';
+import colazero from '../assets/img/drinksimg/cola-zero.png';
+import cocacola from '../assets/img/drinksimg/coca-cola.png';
+import citrustea from '../assets/img/drinksimg/citrus-tea.png';
+import cherryjuice from '../assets/img/drinksimg/cherry-juice.png';
+import cappuucino from '../assets/img/drinksimg/cappuucino.png';
+import blacktea from '../assets/img/drinksimg/black-tea.png';
+import berrylemonade from '../assets/img/drinksimg/berry-lemonade.png';
+import applejuice from '../assets/img/drinksimg/apple-juice.png';
+import berryicetea from '../assets/img/drinksimg/berry-icetea.png';
+
+const drinkImages = {
+  americano,
+  turkishcoffee,
+  strawberrylemonade,
+  strawberryicetea,
+  sprite,
+  sparklingamericano,
+  sirabstill,
+  sirabsparkling,
+  sarikiz,
+  raspberrylemonade,
+  raspberryicetea,
+  pearlemonade,
+  peachjuice,
+  peachicetea,
+  passionfruitlemonade,
+  orangejuice,
+  multijuice,
+  mangolemonade,
+  mangoicetea,
+  limelemonade,
+  limeicetea,
+  lemonlemonade,
+  lemonicetea,
+  latte,
+  jasminetea,
+  icemocha,
+  icelatte,
+  iceamericano,
+  greentea,
+  fanta,
+  espresso,
+  colazero,
+  cocacola,
+  citrustea,
+  cherryjuice,
+  cappuucino,
+  blacktea,
+  berrylemonade,
+  applejuice,
+  berryicetea,
+};
+
+export default drinkImages;
