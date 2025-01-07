6a76017e9779f0a096cea5bc80597fc697041134
Elmir <elmirdevland@gmail.com>
2025-01-08 00:16:43 +0400

    update

--- server/server.js
+++ server/server.js
@@ -10,15 +10,7 @@ const apiToken = process.env.TG_API_TOKEN;
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: 'https://investcafe.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL)
