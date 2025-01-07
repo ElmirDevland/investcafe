7242728348f01f7484a5f3132f2968f4de5b54e1
Elmir <elmirdevland@gmail.com>
2025-01-08 01:23:22 +0400

    test

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const apiToken = process.env.TG_API_TOKEN;
 app.use(express.json());
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://investcafe.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
